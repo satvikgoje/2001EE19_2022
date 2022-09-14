69c666d2d8cff44ef732cb913da89577ccabaafd
Satvik Goje <76347727+satvikgoje@users.noreply.github.com>
2022-09-14 10:39:20 +0530

    tut1 updated

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,10 +1,14 @@
+def octact_identification(mod=5000):
+###Code
 
-def factorial(x):
-    if(x<2):
-        return x
-    else:
-        return factorial(x-1)*x    
 
-x=int(input("Enter the number whose factorial is to be found :"))
-print(factorial(x))
+from platform import python_version
+ver = python_version()
 
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
