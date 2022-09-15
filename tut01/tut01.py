7e0836ef3feee4ec112cd5db35aab4fda3cae430
Satvik Goje <76347727+satvikgoje@users.noreply.github.com>
2022-09-15 21:34:01 +0530

    Added Some Columns And Rows for Mod Count

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -58,8 +58,30 @@ def octact_identification(mod=5000):
         if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] < 0):
             df1.loc[i, "Octant"] = "-4"      ## for -4 
 
-    df1.to_csv('octant_output.csv')
+    # df1.to_csv('octant_output.csv')
+
                          ######  Octant Identification  ########
+
+    df1[" "] = ""  ## creating empty Column without header and assigned "User input" to row 3
+    df1.iloc[1, 11] = "User Input"  
+
+    df1["Octant ID"] = "" ## creating a Coloumn with header as Octant ID 
+    df1.loc[0, "Octant ID"] = "Overall Octant"
+    df1.loc[1, "Octant ID"] = "Mod "+str(mod)
+
+    oct_count = df1['Octant'].value_counts()# oct_count stores a count of unique elements i.e. count of +1,-1,+2,-2,+3,-4,+4 
+    #And assigning a count values to respectively Coloumns
+    df1.loc[0, "+1"] = oct_count["+1"]
+    df1.loc[0, "-1"] = oct_count["-1"]
+    df1.loc[0, "+2"] = oct_count["+2"]
+    df1.loc[0, "-2"] = oct_count["-2"]
+    df1.loc[0, "+3"] = oct_count["+3"]
+    df1.loc[0, "-3"] = oct_count["-3"]
+    df1.loc[0, "+4"] = oct_count["+4"]
+    df1.loc[0, "-4"] = oct_count["-4"]   
+
+    df1.to_csv('octant_output.csv')
+                  ###########   Added Some Columns And Rows for MOD Count   ##########                
 
 
 mod = 5000
