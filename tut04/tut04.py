f85efc94ecc5cc83da93268d0b8e6af356f8d1f5
Satvik Goje <76347727+satvikgoje@users.noreply.github.com>
2022-10-12 02:34:15 +0530

    Tut04: Octant Identification

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,154 +1,85 @@
-import pandas as pd
 
-from datetime import datetime
-start_time = datetime.now()
+try:
+    import pandas as pd
 
+    from datetime import datetime
+    start_time = datetime.now()
 
-def octant_longest_subsequence_count_with_range():
-    # reading the input file
-    df1 = pd.read_excel("input_octant_longest_subsequence_with_range.xlsx")
+    def octant_longest_subsequence_count_with_range():
 
-    avg_u = df1['U'].mean()  # Calculating average of U,V,W
-    avg_v = df1['V'].mean()
-    avg_w = df1['W'].mean()
+        try:
+            # reading the input file
+            df1 = pd.read_excel(
+                "input_octant_longest_subsequence_with_range.xlsx")
 
-    df1["U_Avg"] = ''  # Creating average for coloumns U,V,W
-    df1["V_Avg"] = ''
-    df1["W_Avg"] = ''
-    df1.iloc[0, 4] = avg_u  # assigning the values to respectivley Coloumn
-    df1.iloc[0, 5] = avg_v
-    df1.iloc[0, 6] = avg_w
+            avg_u = df1['U'].mean()  # Calculating average of U,V,W
+            avg_v = df1['V'].mean()
+            avg_w = df1['W'].mean()
 
-    # Creating new coloumns with Header U',V',W'
-    df1["U'=U - U avg"] = df1["U"]-avg_u
-    df1["V'=V - V avg"] = df1["V"]-avg_v
-    df1["W'=W - W avg"] = df1["W"]-avg_w
+            df1["U_Avg"] = ''  # Creating average for coloumns U,V,W
+            df1["V_Avg"] = ''
+            df1["W_Avg"] = ''
+            # assigning the values to respectivley Coloumn
+            df1.iloc[0, 4] = avg_u
+            df1.iloc[0, 5] = avg_v
+            df1.iloc[0, 6] = avg_w
 
-    # df1.to_csv('octant_output.csv')
+            # Creating new coloumns with Header U',V',W'
+            df1["U'=U - U avg"] = df1["U"]-avg_u
+            df1["V'=V - V avg"] = df1["V"]-avg_v
+            df1["W'=W - W avg"] = df1["W"]-avg_w
 
-    #######          Data PreProcessing     ###########
+            # df1.to_csv('octant_output.csv')
 
-    df1["Octant"] = ''  # Creatig a empty Column with Header as Octant
+            #######          Data PreProcessing     ###########
 
-    l = len(df1)  # length of DataFrame = 29745
+            df1["Octant"] = ''  # Creatig a empty Column with Header as Octant
 
-    # creating octant column ,and Identifying the octant value for each triple(U_1,V_1,W_1)
-    for i in range(0, l):
+            l = len(df1)  # length of DataFrame = 29745
 
-        if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] > 0):
-            df1.loc[i, "Octant"] = "+1"  # for +1
+            # creating octant column ,and Identifying the octant value for each triple(U_1,V_1,W_1)
+            for i in range(0, l):
 
-        if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] < 0):
-            df1.loc[i, "Octant"] = "-1"  # for -1
+                if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] > 0):
+                    df1.loc[i, "Octant"] = "+1"  # for +1
 
-        if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] > 0):
-            df1.loc[i, "Octant"] = "+2"  # for +2
+                if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] < 0):
+                    df1.loc[i, "Octant"] = "-1"  # for -1
 
-        if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] < 0):
-            df1.loc[i, "Octant"] = "-2"  # for -2
+                if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] > 0):
+                    df1.loc[i, "Octant"] = "+2"  # for +2
 
-        if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] > 0):
-            df1.loc[i, "Octant"] = "+3"  # for +3
+                if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] > 0 and df1.loc[i, "W'=W - W avg"] < 0):
+                    df1.loc[i, "Octant"] = "-2"  # for -2
 
-        if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] < 0):
-            df1.loc[i, "Octant"] = "-3"  # for -3
+                if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] > 0):
+                    df1.loc[i, "Octant"] = "+3"  # for +3
 
-        if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] > 0):
-            df1.loc[i, "Octant"] = "+4"  # for +4
+                if (df1.loc[i, "U'=U - U avg"] < 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] < 0):
+                    df1.loc[i, "Octant"] = "-3"  # for -3
 
-        if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] < 0):
-            df1.loc[i, "Octant"] = "-4"  # for -4
+                if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] > 0):
+                    df1.loc[i, "Octant"] = "+4"  # for +4
 
-            ######  Octant Identification  ########
+                if (df1.loc[i, "U'=U - U avg"] > 0 and df1.loc[i, "V'=V - V avg"] < 0 and df1.loc[i, "W'=W - W avg"] < 0):
+                    df1.loc[i, "Octant"] = "-4"  # for -4
 
-    df1[" "] = " "
-    df1["Octant "] = " "  # Creating empty column with Octant as a header
-    arr = ["+1", "-1", "+2", "-2", "+3", "-3", "+4", "-4"]
-    for i in range(8):
-        df1.loc[i, "Octant "] = arr[i]  # appending values in octant column
+                    ######  Octant Identification  ########
 
-    df1["Longest Subsequence Length"] = " "
-    df1["Count"] = " "
+            df1.to_excel(
+                'output_octant_longest_subsequence_with_range.xlsx', index=False)
+            # Updating into a output_octant_transition_identify.xlsx file
 
-    l = len(df1)  # 29745
-    x = 0
-    max_count = [0]*8  # initlizing a max_count with all zeroes
-    max_count1 = [0]*8  # initlizing a max_count1 with all zeroes
-    d1 = {"+1": 0, "-1": 1, "+2": 2, "-2": 3,"+3": 4, "-3": 5, "+4": 6, "-4": 7}  # creating a dictionary
-    # To create an empty 2d list of size n
-    time_range = []
-    for i in range(8):
-        time_range.append([])
+        except FileNotFoundError:
+            # if Input file is not found / typo in name of the file
+            print("Error : Input File Not Found")
+        except PermissionError:
+            print("Error: Close the Ouput excel File and run the Code Again")
 
-    while (x < l):
-        s1 = df1.at[x, "Octant"]
-        count = 0
-        j = x
-        while (1):  # counting length of sequence
-            # breaking if next element is not equal to s1
-            if (j >= l or df1.at[j, "Octant"] != s1):
-                break
-            count += 1
-            j += 1
-
-        x += count
-        temp = max_count[d1[s1]]
-
-        # updating a maximum count of value if current count is greater
-        max_count[d1[s1]] = max(max_count[d1[s1]], count)
-
-        if (count > temp):
-            max_count1[d1[s1]] = 1
-            if(len(time_range[d1[s1]])==0):
-                time_range[d1[s1]].append(j-1)
-
-            else:
-                time_range[d1[s1]].clear()
-                time_range[d1[s1]].append(j-1)
-            
-                
-            
-        if (count == temp):
-            max_count1[d1[s1]] += 1
-            time_range[d1[s1]].append(j-1)
-
-    for i in range(8):
-        # updating Longest subsequence length for respectively octant values
-        df1.loc[i, "Longest Subsequence Length"] = max_count[i]
-
-    for j in range(8):
-        # updating count of LSL for respectively octant values
-        df1.loc[j, "Count"] = max_count1[j]
-
-    df1["  "] = " "
-    df1[" Octant "] = " "
-    df1[" Longest Subsequence Length"] = " "
-    df1[" Count"] = " "
-    print(time_range)
-
-    t=0
-    for i in range(8):
-        df1.loc[t," Octant "]=arr[i]
-        df1.loc[t," Longest Subsequence Length"]=max_count[i]
-        df1.loc[t," Count"]=max_count1[i]
-        t+=1
-        df1.loc[t," Octant "]="Time"
-        df1.loc[t," Longest Subsequence Length"]="From"
-        df1.loc[t," Count"]="To"
-        t+=1
-        for j in range(max_count1[i]):
-            df1.loc[t," Longest Subsequence Length"]=0.01*(time_range[d1[arr[i]]][j])-0.01*(max_count[i]-1)
-            df1.loc[t," Count"]=0.01*time_range[d1[arr[i]]][j]
-            t+=1
-
-
-    df1.to_excel('octant_output1.xlsx', index=False)
-    
-
-
+except:
+    print("Error:Install Pandas library ")
 
 octant_longest_subsequence_count_with_range()
 # This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
-
