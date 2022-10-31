39b24d6125d85c608716416d528a2ab186ee08d9
Satvik Goje <76347727+satvikgoje@users.noreply.github.com>
2022-10-31 22:11:19 +0530

    Tut 05: MOD ranges value updated

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -63,13 +63,69 @@ try:
 
                     ######  Octant Identification  ########
 
+            # creating empty Column without header and assigned "User input" to row 3
+            df1[" "] = ""
+            df1.iloc[1, 11] = "User Input"
+
+            df1["Octant ID"] = " "  # creating a Coloumn with header as Octant ID
+            df1.loc[0, "Octant ID"] = "Overall Octant"
+            df1.loc[1, "Octant ID"] = "Mod "+str(mod)
+
+            # oct_count stores a count of unique elements i.e. count of +1,-1,+2,-2,+3,-4,+4
+            oct_count = df1['Octant'].value_counts()
+            
+            arr = ["+1", "-1", "+2", "-2", "+3", "-3", "+4", "-4"] #cretaed for reference
+
+            oct_cnt = {} #for storing octant count as key and and coreesponding octant value as value in dict 
+            for i in range(8):
+                s = arr[i] 
+                df1.loc[0, s] = oct_count[s]# And assigning a count values to respectively Coloumns
+
+            octant_name_id_mapping = {"1": "Internal outward interaction", "-1": "External outward interaction", "2": "External Ejection",
+                                    "-2": "Internal Ejection", "3": "External inward interaction", "-3": "Internal inward interaction", "4": "Internal sweep", "-4": "External sweep"}
+
+                    ###########   Added Some Columns And Rows for MOD Count   ##########
+
+            x = 0  # for findind octant values for MOD ranges
+            t = 2  # for row pointer
+
+            while (x < l):
+
+                d1 = {"+1": 0, "-1": 1, "+2": 2, "-2": 3, "+3": 4,
+                    "-3": 5, "+4": 6, "-4": 7}  # creating a dictionary for reference
+
+                oct_cnt_mod = [0]*8 # count values of each octant is stored for MOD ranges
+
+                for i in range(x, x+mod, 1):
+
+                    if (i >= l):
+                        break  # bound check
+                    s3 = df1.at[i, "Octant"]
+                    oct_cnt_mod[d1[s3]] += 1 #incrementing by one of count values of corresponding octants 
+
+                for i in range(8):
+                    s = arr[i]
+                    # assigning overall count of octants in each interval
+                    df1.loc[t, s] = oct_cnt_mod[i]
+            
+                if ((x+mod) > l):  # Writing MOD ranges in Octant ID Coloumn
+                    df1.loc[t, "Octant ID"] = str(x)+"-"+str(l-1)  # for last index(i.e) 2744
+                else:
+                    df1.loc[t, "Octant ID"] = str(x)+"-"+str(x+mod-1)
+
+                x += mod
+                t += 1
+
+                ################ Octant Count Based on Mod Values  ######################
+
+
             df1.to_excel('octant_output_ranking_excel.xlsx', index=False)
 
         except FileNotFoundError:
             # if Input file is not found / typo in name of the file
             print("Error : Input File Not Found")
         except PermissionError:
-            print("Error: Dont have the permission ,Close the output excel file and code the code again")
+             print("Error: Dont have the permission ,Close the output excel file and code the code again")
         except ZeroDivisionError:
             print("Enter valid mod value") #if mod value is invalid   
 
