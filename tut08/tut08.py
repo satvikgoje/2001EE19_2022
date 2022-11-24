92977a8f46cad83aa15c005afc9c6eed2ef83771
Satvik Goje <76347727+satvikgoje@users.noreply.github.com>
2022-11-24 20:05:26 +0530

    tut08-Question Updated

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,21 +1,26 @@
 
-
 from datetime import datetime
 start_time = datetime.now()
 
-def attendance_report():
+#Help
+def scorecard():
+	pass
+
+
 ###Code
 
 from platform import python_version
 ver = python_version()
 
 if ver == "3.8.10":
-    print("Correct Version Installed")
+	print("Correct Version Installed")
 else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
-attendance_report()
+scorecard()
+
+
 
 
 
